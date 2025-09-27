f3aad790dd5f6a17d39247545da960a5e2ec5357
soleyhyman <soleyhyman@arizona.edu>
2025-09-26 19:55:10 -0700

    added placeholder docstrings and two class methods

--- src/core.py
+++ src/core.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class quadratic:
-    # FINISH DOCSTRING FOR CLASS
     """
-    Add description of class here and format the quadratic equation (i.e., y = ax^2 + bx + c) in rst/LaTeX 
+    Add description of class here and format the quadratic equation (i.e., y = ax^2 + bx + c) in rst/LaTeX. 
 
     Attributes
     ----------
-    Add the attributes, which in this case are the same as the __init__ parameters
+    Add the attributes, which IN THIS CASE are the same as the __init__ parameters
+    (if other functions set attributes, you'll need to add them here too)
 
     Examples
     --------
-    Add an example of how to use the class with a >>> code marker and the printout result below it
+    Add an example of how to use the class with a >>> code marker and the printout result below it.
     """
     def __init__(self, a, b, c):
         """
@@ -28,6 +28,22 @@ class quadratic:
         self.a = a
         self.b = b
         self.c = c
+
+    def __call__(self, x):
+        """
+        _summary_
+
+        Parameters
+        ----------
+        x : _type_
+            _description_
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
+        return self.a*x**2 + self.b*x + self.c
     
     def roots(self):
         """
@@ -55,17 +71,45 @@ class quadratic:
         return self.c
     
     def vertex(self):
-        # ADD DOCSTRING FOR VERTEX CALCULATION
+        """
+        _summary_
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         foura = 4.*self.a
         D = self.b*self.b - foura*self.c
         return np.array([-self.b/(2.*self.a),-D/(foura)])
     
-    def quadEq(self, x):
-        # ADD DOCSTRING FOR QUADRATIC EQUATION
-        return self.a*x**2 + self.b*x + self.c
-    
     def plot(self, x, roots=True, originAxes=True, showPlot=True, styleKwargs={"color":"k", "lw":1}):
-        # ADD DOCSTRING FOR THE PLOTTING FUNCTION
+        """
+        _summary_
+
+        Parameters
+        ----------
+        x : _type_
+            _description_
+        roots : bool, optional
+            _description_, by default True
+        originAxes : bool, optional
+            _description_, by default True
+        showPlot : bool, optional
+            _description_, by default True
+        styleKwargs : dict, optional
+            _description_, by default {"color":"k", "lw":1}
+
+        Returns
+        -------
+        _type_
+            _description_
+
+        Raises
+        ------
+        TypeError
+            _description_
+        """
         x = np.array([x])
         if np.issubdtype(x.dtype,np.number) and x.ndim==1 and x.size>=3:
             pass
